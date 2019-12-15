2f61487519303e67430ab872b8ba248faefdd8fb
Ohata Kenji <swordgram@gmail.com>
2019-12-15 13:59:08 +0900

    UPDATE  params for test

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ from pathlib import Path
 
 data_path = Path.home() / 'MyDatasets/Sketches/apple/test.npy'
 
-dataset = V5Dataset(str(data_path), To5vStrokes(max_len=200))
+dataset = V5Dataset(str(data_path), To5vStrokes(max_len=80))
 dataloader = torch.utils.data.DataLoader(
-    dataset, batch_size=100, shuffle=True)
+    dataset, batch_size=10, shuffle=True)
 
-model = SketchRNN(enc_hidden_size=256, dec_hidden_size=256,
-                  Nz=128, M=20, dropout=0.9)
+model = SketchRNN(enc_hidden_size=64, dec_hidden_size=64,
+                  Nz=64, M=5, dropout=0.1)
 trainer = Trainer(model, dataloader, learning_rate=0.0001)
 
 trainer.train(epoch=3)
