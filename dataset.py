345cdff06b047d0d75fe866d5f9759b0385204e1
Ivan <ivan.puhachov@gmail.com>
2021-03-28 19:01:03 -0400

    dataset fixed

--- dataset.py
+++ dataset.py
@@ -1,8 +1,6 @@
 import numpy as np
 import torch
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 
 class To5vStrokes():
     def __init__(self, max_len=200):
@@ -20,31 +18,43 @@ class To5vStrokes():
         result[0:l, 3] = stroke[:, 2]
         result[0:l, 2] = 1 - result[0:l, 3]
         result[l:, 4] = 1
-        return torch.from_numpy(result).float().to(device)
+        return torch.from_numpy(result).float()
 
 
 class V5Dataset(torch.utils.data.Dataset):
-    def __init__(self, data_path, transform=None, pre_scaling=True):
+    def __init__(self, data_array, transform=None, pre_scaling=True):
         super().__init__()
         self.transform = transform
-        self.data = np.load(data_path, encoding='bytes', allow_pickle=True)
-        scale = self.scaling_factor()
-        self.data = np.array(list(
-            map(lambda x: self.scale_stroke(x, scale), self.data)))
+        print("Processing data: ", data_array.shape)
+        self.data = [x.astype('float32') for x in data_array]
+        self.true_lengths = [x.shape[0] for x in data_array]
+        if pre_scaling:
+            scale = self.scaling_factor(self.data)
+            self.data = [self.scale_stroke(x, scale) for x in self.data]
         if transform is not None:
-            self.data = np.array(list(map(transform, self.data)))
+            for i in range(len(self.data)):
+                self.data[i] = transform(self.data[i])
 
     def __len__(self):
-        return self.data.shape[0]
+        return len(self.data)
 
     def __getitem__(self, index):
-        return self.data[index], 0
+        return self.data[index], self.true_lengths[index]
 
-    def scaling_factor(self):
-        data = np.concatenate([S for S in self.data])
+    def scaling_factor(self, datalist):
+        data = np.concatenate([S for S in datalist])
         return np.std(data[:, 0:2])
 
     def scale_stroke(self, x, scale):
         x = np.float32(x)
         x[:, 0:2] /= scale
         return x
+
+
+def load_quickdraw_datasets(path_to_npz):
+    a = np.load(path_to_npz, encoding='latin1', allow_pickle=True)
+    trainset = V5Dataset(a['train'], To5vStrokes(), pre_scaling=True)
+    testset = V5Dataset(a['test'], To5vStrokes(), pre_scaling=True)
+    valset = V5Dataset(a['valid'], To5vStrokes(), pre_scaling=True)
+
+    return trainset, testset, valset
