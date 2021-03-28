edc4a86e916d332e3359626985e5a6bb29f6dd76
Ivan <ivan.puhachov@gmail.com>
2021-03-28 19:03:52 -0400

    running test

--- test.py
+++ test.py
@@ -1,24 +1,34 @@
 import torch
-from dataset import To5vStrokes, V5Dataset
+from dataset import load_quickdraw_datasets
 from model import SketchRNN
 from trainer import Trainer
-from pathlib import Path
 from torch.utils.tensorboard import SummaryWriter
+import os
+import shutil
 
-data_path = 'data/train.npy'
-dataset = V5Dataset(str(data_path), To5vStrokes(max_len=200), pre_scaling=True)
+trainset, testset, valset = load_quickdraw_datasets("data/owl.npz")
+
 dataloader = torch.utils.data.DataLoader(
-    dataset, batch_size=100, shuffle=True)
+    trainset, batch_size=100, shuffle=True, num_workers=8)
 
-log_dir = 'log/'
+log_dir = 'logs4/'
+checkpoint_dir = log_dir + 'checkpoints/'
+
+if os.path.exists(log_dir):
+    print("-- Clearing logs folder --")
+    shutil.rmtree(log_dir)
+
+os.mkdir(log_dir)
+os.mkdir(checkpoint_dir)
+
 tb_writer = SummaryWriter(log_dir)
 
-checkpoint_dir = 'checkpoints/'
 model = SketchRNN(enc_hidden_size=256, dec_hidden_size=512,
                   Nz=128, M=20, dropout=0.1)
+print("CUDA: ", torch.cuda.is_available())
+
 trainer = Trainer(model, dataloader, tb_writer,
                   checkpoint_dir, learning_rate=0.0001)
 
-trainer.train(epoch=300)
-
+trainer.train(epoch=20)
 tb_writer.close()
